35c4096b08d565c623d1b5234926faaff6871b08
Marcus <marcus.gamboa@gmail.com>
2021-08-06 12:08:28 -0600

    add message board functionality

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Mini Message Board
+# Mini Message Board
 
 [Project](https://www.theodinproject.com/paths/full-stack-javascript/courses/nodejs/lessons/mini-message-board) provided by The Odin Project.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+/* GET new message page. */
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+/* POST new message page. */
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
